abd8cab0cd4ac1af709bd2508645291d1a9eaa69
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-04-29 17:14:03 +0530

    Time: 6 ms (28.43%) | Memory: 43 MB (6.33%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -2,16 +2,14 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         String processed = "";
         int x = 0;
-        int y= 0;
-        while(x!=word1.length() || y!=word2.length()){
-            if(x!=word1.length()){
+        while(x<word1.length() || x<word2.length()){
+            if(x<word1.length()){
                 processed+=word1.charAt(x);
-                x++;
             }
-            if(y!=word2.length()){
-                processed+=word2.charAt(y);
-                y++;
+            if(x<word2.length()){
+                processed+=word2.charAt(x);
             }
+            x++;
         }
 
         return processed;
