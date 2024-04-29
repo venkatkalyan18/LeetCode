df359938478ab7618d7d5370dbf150ddaab7b52e
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-04-29 19:17:21 +0530

    Time: 5 ms (41.84%) | Memory: 44.8 MB (48.89%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,19 +1,11 @@
 class Solution {
     public String reverseVowels(String s) {
-        List<Character> vowels = new ArrayList<>();
-        
-        // Array of vowels
-        char[] eachVowels = {'a','e','i','o','u','A','E','I','O','U'};
-
-        // Add all elements from the array to the list
-        for (char vowel : eachVowels) {
-            vowels.add(vowel);
-        }
+        String vowels = "aeiouAEIOU";
          StringBuilder sb = new StringBuilder(s);
          int i=0;
          int j=s.length()-1;
          while(i<j){
-            if(vowels.contains(sb.charAt(i)) && vowels.contains(sb.charAt(j))){
+            if(vowels.indexOf(sb.charAt(i)) != -1&& vowels.indexOf(sb.charAt(j)) != -1){
                 if(sb.charAt(i) != sb.charAt(j)){
                     char temp = sb.charAt(i);
                     sb.setCharAt(i,sb.charAt(j));
@@ -22,7 +14,7 @@ class Solution {
                 i++;
                 j--;
             }
-                else if(!vowels.contains(sb.charAt(i))){
+                else if(vowels.indexOf(sb.charAt(i)) == -1){
                     i++;
                 }else{
                     j--;
