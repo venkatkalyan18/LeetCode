81799646354fc725ff59309e1e3b751a96a3a7a4
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-05-28 19:33:03 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (95.65%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int[] smallLettersArray = new int[26];
+        int[] capitalLettersArray = new int[26];
+        int ans = 0;
+        for(int i=0;i<word.length();i++){
+            int index =0;
+            if(word.charAt(i) >= 65 && word.charAt(i)<=90){
+                index = word.charAt(i)-'A';
+                capitalLettersArray[index]=1;
+            }else{
+                index = word.charAt(i)-'a';
+                smallLettersArray[index]=1;
+            }
+            
+     
+        }
+
+        for(int i=0;i<26;i++){
+            if(smallLettersArray[i] == 1 && capitalLettersArray[i]==1){
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
