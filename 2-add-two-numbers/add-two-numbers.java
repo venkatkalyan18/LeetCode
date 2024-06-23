f7166463ee2e9195f0e4365722cf3bdd1326618e
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-06-23 10:40:36 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (58.40%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode curr = head;
+        int carry = 0;
+        while(l1 != null && l2!= null){
+            int sum = l1.val+l2.val+carry;
+            carry = 0;
+            if(sum > 9){
+                carry = sum / 10;
+                sum = sum % 10;
+                
+            }
+
+            ListNode temp = new ListNode(sum);
+            curr.next = temp;
+            curr = curr.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while(l1 != null){
+            int sum = l1.val+carry;
+            carry = 0;
+            if(sum > 9){
+                carry = sum / 10;
+                sum = sum % 10;
+                
+            }
+
+            ListNode temp = new ListNode(sum);
+            curr.next = temp;
+            curr = curr.next;
+             l1 = l1.next;
+        }
+
+         while(l2 != null){
+            int sum = l2.val+carry;
+            carry = 0;
+            if(sum > 9){
+                 carry = sum / 10;
+                sum = sum % 10;
+               
+            }
+
+             ListNode temp = new ListNode(sum);
+            curr.next = temp;
+                 curr = curr.next;
+             l2 = l2.next;
+        }
+
+        if(carry != 0){
+            ListNode temp = new ListNode(carry);
+            curr.next = temp;
+            curr = curr.next;
+        }
+
+        curr.next = null;
+
+        return head.next;
+    }
+}
