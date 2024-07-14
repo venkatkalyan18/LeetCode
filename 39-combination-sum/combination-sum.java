3d7428894a707c855ac4c3633837c975c1563e87
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-07-14 16:56:38 +0530

    Time: 3 ms (33.68%) | Memory: 44.6 MB (41.29%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(candidates,0,target,ans,new ArrayList<>());
+        return ans;
+    }
+
+    public void helper(int[] arr,int i,int target,List<List<Integer>> ans,List<Integer> list){
+        if(i == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+
+        if(target < 0){
+            return;
+        }
+
+        list.add(arr[i]);
+        helper(arr,i,target - arr[i],ans,list);
+        list.remove(list.size()-1);
+        helper(arr,i+1,target,ans,list);
+    }
+}
