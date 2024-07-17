fcb07c63c00ab9287b449e6346d01d71ddc32297
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-07-17 10:57:02 +0530

    Time: 3 ms (61.35%) | Memory: 44.9 MB (33.33%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        if(n == 1){
+            List<String> li = new ArrayList<>();
+            li.add("Q");
+            List<List<String>> result = new ArrayList<>();
+            result.add(new ArrayList<>(li));
+            return result;
+        }
+        List<List<String>> ans = new ArrayList<>();
+        boolean[][] board = new boolean[n][n];
+        helper(n,0,board,new ArrayList<>(),ans);
+        return ans;
+    }
+
+    public void helper(int n,int row,boolean[][] board,List<String> list,List<List<String>> ans){
+        if(row == n){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for(int col = 0;col<n;col++){
+            if(check(board,row,col)){
+                board[row][col] = true;
+                char[] chArray = new char[n];
+                Arrays.fill(chArray,'.');
+                chArray[col] = 'Q';
+                list.add(new String (chArray));
+                helper(n,row+1,board,list,ans);
+                board[row][col] = false;
+                list.remove(list.size()-1); 
+            }
+        }
+    }
+    public boolean check(boolean board[][],int row,int col){
+        for(int i=0;i<row;i++){
+            if(board[i][col]){
+                return false;
+            }
+        }
+
+        //check left
+
+        int maxLeft = Math.min(row,col);
+        for(int i=1;i<=maxLeft;i++){
+            if(board[row-i][col-i]){
+                return false;
+            }
+        }
+
+        int maxRight = Math.min(board.length - col -1 ,row);
+        for(int i=1;i<=maxRight;i++){
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
