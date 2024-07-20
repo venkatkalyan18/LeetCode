a821a9603a4b8d10185f332bdee85e410547a064
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-07-20 14:52:06 +0530

    Time: 1 ms (86.36%) | Memory: 41 MB (14.23%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int i = 1;
+        int j = x;
+        int ans = 0;
+        while (i <= j) {
+            int mid = i + (j - i) / 2; 
+            if ((long) mid * mid == x) {
+                return mid;
+            }
+            if ((long) mid * mid > x) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+}
