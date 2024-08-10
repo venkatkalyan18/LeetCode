cc49c428e32e1f04b890b010ef097af52cc84749
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-08-11 00:46:22 +0530

    Time: 17 ms (75.06%) | Memory: 45.5 MB (31.37%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -5,22 +5,21 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         StringBuilder sb = new StringBuilder(s);
         
-        // First pass: Identify invalid parentheses
+
         for (int i = 0; i < sb.length(); i++) {
             char ch = sb.charAt(i);
             if (ch == '(') {
                 st.push(i);
             } else if (ch == ')') {
                 if (!st.isEmpty()) {
-                    st.pop();  // Valid match, pop from the stack
+                    st.pop();  
                 } else {
-                    sb.deleteCharAt(i);  // No match for ')', so remove it
-                    i--;  // Adjust the index after deletion
+                    sb.deleteCharAt(i); 
+                    i--;  
                 }
             }
         }
-        
-        // Second pass: Remove unmatched '('
+
         while (!st.isEmpty()) {
             sb.deleteCharAt(st.pop());
         }
