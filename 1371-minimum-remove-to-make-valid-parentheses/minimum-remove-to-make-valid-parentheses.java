e90fb736137c99f0d0183dadf8459080eb5d9a7f
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-08-11 00:52:37 +0530

    Time: 16 ms (80.34%) | Memory: 45.5 MB (42.94%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,25 +1,27 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public String minRemoveToMakeValid(String s) {
+        int n = s.length();
         Stack<Integer> st = new Stack<>();
         StringBuilder sb = new StringBuilder(s);
         
-
-        for (int i = 0; i < sb.length(); i++) {
+        for (int i = 0; i < n; i++) {
             char ch = sb.charAt(i);
             if (ch == '(') {
                 st.push(i);
             } else if (ch == ')') {
-                if (!st.isEmpty()) {
-                    st.pop();  
+                if (st.isEmpty()) {
+                    sb.deleteCharAt(i);
+                    i--;  // Adjust the index after deletion
+                    n--;  // Adjust the length after deletion
                 } else {
-                    sb.deleteCharAt(i); 
-                    i--;  
+                    st.pop();
                 }
             }
         }
-
+        
+        // Remove any unmatched '(' left in the stack
         while (!st.isEmpty()) {
             sb.deleteCharAt(st.pop());
         }
