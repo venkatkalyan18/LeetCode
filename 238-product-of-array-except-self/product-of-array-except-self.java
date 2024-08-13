1b452f2e0f61fdaef36fc0f11caeb86b06f72fe9
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-08-13 22:13:54 +0530

    Time: 3 ms (23.29%) | Memory: 55.5 MB (47.31%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -12,9 +12,6 @@ class Solution {
             left[i] = l;
             right[nums.length-1-i] = r;
         }
-        for(int i=0;i<nums.length;i++){
-            System.out.println(right[i]);
-        }
 
         nums[nums.length-1] = left[left.length-2];
         nums[0] = right[1];
