dc02051861ab4e4674e8c9be16ba052f7e14e2e1
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-08-17 21:28:38 +0530

    Time: 6 ms (32.55%) | Memory: 44.4 MB (53.73%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,48 +1,32 @@
 class Solution {
     public String intToRoman(int num) {
-       HashMap<Integer, String> map = new HashMap<>();
-map.put(1, "I");
-map.put(5, "V");
-map.put(10, "X");
-map.put(50, "L");
-map.put(100, "C");
-map.put(500, "D");
-map.put(1000, "M");
-map.put(4, "IV");
-map.put(9, "IX");
-map.put(40, "XL");
-map.put(90, "XC");
-map.put(400, "CD");
-map.put(900, "CM");
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+        map.put(1000, "M");
 
-        int[] arr = new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String str = num + "";
-        String ans = "";
-        for(int i=0;i<str.length();i++){
-            String num1 = str.charAt(i) + "";
-            for(int j=i;j<str.length()-1;j++){
-                num1 +='0';
-            }
-            int n = Integer.parseInt(num1);
-            if(map.containsKey(n)){
-                ans+=map.get(n);
-            }else{
-                ans += getRoman(n,arr,map);
-            }
-        }
-        return ans;
-    }
-
-    public String getRoman(int n,int[] arr,HashMap<Integer,String> map){
-        String ans = "";
+        int[] arr = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        StringBuilder ans = new StringBuilder();
         int i = 0;
-        while(n > 0){
-            while(n> 0 && arr[i] <= n){
-                n-=arr[i];
-                ans+=map.get(arr[i]);
+        while(num > 0){
+            while(num > 0 && num >= arr[i]){
+                ans.append(map.get(arr[i]));
+                num-=arr[i];
             }
             i++;
         }
-        return ans;
+
+        return ans.toString();
     }
 }
