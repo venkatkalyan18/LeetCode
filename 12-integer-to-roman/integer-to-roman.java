ec089627cf94b4af06b62797b1ef1f23b8096835
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2024-08-17 21:29:04 +0530

    Time: 3 ms (97.73%) | Memory: 44 MB (93.96%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,32 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(900, "CM");
-        map.put(1000, "M");
-
-        int[] arr = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        
-        StringBuilder ans = new StringBuilder();
-        int i = 0;
-        while(num > 0){
-            while(num > 0 && num >= arr[i]){
-                ans.append(map.get(arr[i]));
-                num-=arr[i];
+        String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ;i<values.length;i++)
+        {
+            while(num>=values[i])
+            {
+                num-=values[i];
+                sb.append(romanSymbols[i]);
             }
-            i++;
         }
-
-        return ans.toString();
+        return sb.toString();
     }
 }
