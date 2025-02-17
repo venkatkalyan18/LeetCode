6d41069a17475dbe0b8f3ec50dbb2417bb6ba87e
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-02-17 16:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (39.86%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.java
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.java
@@ -1,45 +1,42 @@
 class Solution {
     public int[] constructDistancedSequence(int n) {
-        int[] sequence = new int[n*2 - 1];
-        boolean[] used = new boolean[n + 1];
-        
-        backtrack(sequence, used, n, 0);
-        return sequence;
+        boolean[] isUsed = new boolean[n + 1];
+        int[] res = new int[2 * n - 1];
+        helper(n, res, isUsed, 0);
+        return res;
     }
 
-    public boolean backtrack(int[] sequence, boolean[] used, int n, int index) {
-        if(index == sequence.length){
-            return true;
-        }
+    public boolean helper(int n, int[] res, boolean[] isUsed, int idx) {
+        if (idx == res.length) return true;
 
-        if(sequence[index] != 0){
-            return backtrack(sequence, used, n, index+1);
-        }
+        if (res[idx] != 0) return helper(n, res, isUsed, idx + 1); 
 
-        for(int num = n; num >= 1; num--){
-            if(used[num])   continue;
+        for (int i = n; i > 0; i--) { 
+            if (i != 1 && !isUsed[i] && idx + i < res.length && res[idx + i] == 0) {
+                isUsed[i] = true;
+                res[idx] = i;
+                res[idx + i] = i; 
 
-            if(num == 1){
-                sequence[index] = 1;
-                used[num] = true;
-                if(backtrack(sequence, used, n, index+1))    return true;
-                sequence[index] = 0;
-                used[num] = false;
-            } else {
-                int secondIndex = index + num;
-                if(secondIndex < sequence.length && sequence[secondIndex] == 0){
-                    sequence[index] = num;
-                    sequence[secondIndex] = num;
-                    used[num] = true;
-
-                    if (backtrack(sequence, used, n, index + 1))    return true;
-
-                    sequence[index] = 0;
-                    sequence[secondIndex] = 0;
-                    used[num] = false;
+                if (helper(n, res, isUsed, idx + 1)) {
+                    return true;
                 }
+
+                isUsed[i] = false;
+                res[idx] = 0;
+                res[idx + i] = 0; 
+            } else if (i == 1 && !isUsed[i]) {
+                isUsed[i] = true;
+                res[idx] = i;
+
+                if (helper(n, res, isUsed, idx + 1)) {
+                    return true;
+                }
+
+                isUsed[i] = false;
+                res[idx] = 0;
             }
         }
+
         return false;
     }
 }
