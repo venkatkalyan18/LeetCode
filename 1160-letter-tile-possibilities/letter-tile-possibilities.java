ac2c4fd25ee977a34265cedb00443df59a72e44b
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-02-17 15:47:20 +0530

    Time: 2 ms (87.75%) | Memory: 41.7 MB (69.46%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,25 +1,25 @@
-import java.util.Arrays;
-
 class Solution {
     public int numTilePossibilities(String tiles) {
-        char[] arr = tiles.toCharArray();
-        Arrays.sort(arr); 
-        return helper(arr, new boolean[arr.length]);
+         char[] arr = tiles.toCharArray();
+        Arrays.sort(arr);
+        return helper(new String(arr),new boolean[tiles.length()]);
     }
 
-    public int helper(char[] tiles, boolean[] isUsed) {
-        int count = 0;
-        for (int i = 0; i < tiles.length; i++) {
-            if (isUsed[i]) continue;
+    public int helper(String tiles,boolean[] isUsed){
 
-            if (i > 0 && tiles[i] == tiles[i - 1] && !isUsed[i - 1]) {
-                continue;
+        int count = 0;
+        for(int i = 0;i<tiles.length();i++){
+            if(!isUsed[i]){
+                isUsed[i] = true;
+                count += (1 + helper(tiles,isUsed));
+                isUsed[i] = false;
+                while( i < tiles.length() -1 && tiles.charAt(i) == tiles.charAt(i+1)  ){
+                     i++;
+                }
             }
 
-            isUsed[i] = true;
-            count += 1 + helper(tiles, isUsed);
-            isUsed[i] = false;
         }
+
         return count;
     }
 }
