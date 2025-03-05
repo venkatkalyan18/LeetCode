e169d2f222ec8328843101980095339e78206f7d
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-03-05 15:35:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (16.39%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -3,16 +3,10 @@ class Solution {
         if(n == 1){
             return 1;
         }
+        n--;
+        return 1 + 4*((long)n*(n + 1) / 2);
 
-        if(n == 2){
-            return 5;
-        }
-        long sum = 0;
-        for(int i = 3;i<=n;i++){
-            sum += 4*(i-1);
-        }
-
-        return sum + 5;
+        
     }
 }
     
