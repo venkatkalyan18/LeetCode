322ee4b0d807587c2c9eb344e53478703dd9a836
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-03-25 18:02:22 +0530

    Time: 1007 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+select  a2.machine_id
+, ROUND(AVG(a2.timestamp - a1.timestamp),3) as processing_time
+from Activity a1
+cross join Activity a2
+on a1.process_id=a2.process_id
+and a1.machine_id=a2.machine_id
+and a1.timestamp<a2.timestamp
+group by a1.machine_id;
