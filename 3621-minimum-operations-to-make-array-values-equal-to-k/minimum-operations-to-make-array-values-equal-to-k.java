8fde2069dd81780bf7aff5745b218965e1db558b
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-04-09 19:20:14 +0530

    Time: 2 ms (82.71%) | Memory: 43.9 MB (100.00%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,31 +1,25 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int res = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        Set<Integer> s = new HashSet<>();
-        for(int i : nums){
-            if(!s.contains(i)){
-                            pq.add(i);
-                            s.add(i);
-            }
-
-        }
-
-        int maxi = pq.poll();;
-        while(!pq.isEmpty()){
-            int temp = pq.poll();
-            maxi = temp;
+       Arrays.sort(nums);
+       int res = 0;
+       int i = 0;
+       while(i<nums.length){
             res++;
-        }
+            int temp = nums[i];
+            while(i < nums.length && temp == nums[i]){
+                i++;
+            }
+       }
 
-        if(maxi == k){
-            return res;
-        }
-        System.out.println(maxi);
-        if(k < maxi){
-            return res+1;
-        }
+       
+       if(nums[0] == k){
+            return res-1;
+       }
 
-        return -1;
+       if(nums[0] > k){
+            return res ;
+       }
+
+       return -1;
     }
 }
