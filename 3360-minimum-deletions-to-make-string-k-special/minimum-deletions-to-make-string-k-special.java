b17b66cbcf03667bc8350cd1e61c023677892047
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-06-21 18:33:04 +0530

    Time: 36 ms (12.77%) | Memory: 45.9 MB (17.02%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 class Solution {
     public int minimumDeletions(String w, int k) {
-        // Frequency map
         HashMap<Character, Integer> map = new HashMap<>();
         for (char ch : w.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
         List<Integer> freqList = new ArrayList<>(map.values());
-        Collections.sort(freqList); 
+
 
         int res = Integer.MAX_VALUE;
         for (int i = 0; i < freqList.size(); i++) {
