68b573fe9eb431e5652e34cacb78e9819d0f3af1
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-06-21 19:46:51 +0530

    Time: 3 ms (99.60%) | Memory: 64 MB (40.48%) - LeetSync

--- 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.java
+++ 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.java
@@ -1,37 +1,44 @@
 class Solution {
     public int maxSumDistinctTriplet(int[] x, int[] y) {
-        List<Pair> li = new ArrayList<>();
-        for(int i=0;i<x.length;i++){
-            li.add(new Pair(x[i],y[i]));
-        }
-        Collections.sort(li,(a,b) -> b.y - a.y);
-        int count = 1;
-        int sum = li.get(0).y;
-        int[] set = new int[4];
-        set[0] = li.get(0).x;
-        for(int i=1;i<li.size() && count < 3;i++){
-            while(i < li.size() && (set[0] == li.get(i).x || set[1] == li.get(i).x)){
-                i++;
+        int idx1 = -1, idx2 = -1, idx3 = -1;
+
+        for (int i = 0; i < x.length; i++) {
+            if (idx1 == -1) {
+                idx1 = i;
+            } else if (x[i] == x[idx1]) {
+                if (y[i] > y[idx1]) {
+                    idx1 = i;
+                }
+            } else if (idx2 == -1) {
+                idx2 = i;
+            } else if (x[i] == x[idx2]) {
+                if (y[i] > y[idx2]) {
+                    idx2 = i;
+                }
+            } else if (idx3 == -1) {
+                idx3 = i;
+            } else if (x[i] == x[idx3]) {
+                if (y[i] > y[idx3]) {
+                    idx3 = i;
+                }
+            } else {
+                int minY = Math.min(y[idx1], Math.min(y[idx2], y[idx3]));
+                if (y[i] > minY) {
+                    if (y[idx1] == minY) {
+                        idx1 = i;
+                    } else if (y[idx2] == minY) {
+                        idx2 = i;
+                    } else {
+                        idx3 = i;
+                    }
+                }
             }
-            if(li.size() == i){
-                return -1;
-            }
-            set[count] = li.get(i).x;
-            count++;
-            sum += li.get(i).y;
         }
-        if(count != 3){
+
+        if (idx1 == -1 || idx2 == -1 || idx3 == -1) {
             return -1;
         }
-        return sum;
-    }
 
-    class Pair{
-        int x;
-        int y;
-        Pair(int x,int y){
-            this.x = x;
-            this.y = y;
-        }
+        return y[idx1] + y[idx2] + y[idx3];
     }
 }
