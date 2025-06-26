1076f69e97edc2b0b1335259dcfaf9b44ed083da
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-06-26 15:22:55 +0530

    Time: 10 ms (11.70%) | Memory: 57.9 MB (5.32%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.java
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.java
@@ -1,19 +1,30 @@
 class Solution {
     public int longestSubsequence(String s, int k) {
-        int res = 0;
-        int currVal = 0;
-        int n = s.length() - 1;
-        for (int i = n; i >= 0; i--) {
-            if (s.charAt(i) == '1') {
-                int val = (int) Math.pow(2, n - i);
-                if ((long) currVal + val <= (long) k) {
-                    res++;
-                    currVal += val;
-                }
-            } else {
-                res++;
-            }
+        Integer[][] dp = new Integer[s.length()][s.length()];
+        return helper(s,k,0,0,s.length()-1,s.length()-1,dp);
+    }
+
+    public int helper(String s,int k,int currSum,int size,int i,int n,Integer[][] dp){
+        if(i < 0){
+            return 0;
         }
-        return res;
+        if(dp[i][size] != null){
+            return dp[i][size];
+        }
+        int take = 0;
+        int notTake = 0;
+        if(s.charAt(i) == '0'){
+            take = 1 + helper(s,k,currSum,size+1,i-1,n,dp);
+        }else{
+            int val = (int) Math.pow(2,n-i);
+            if((long) currSum + val <= (long) k){
+                take = 1 + helper(s,k,currSum+val,size+1,i-1,n,dp);
+            }
+        }  
+
+        if(s.charAt(i) == '1'){
+            notTake = helper(s,k,currSum,size+1,i-1,n,dp);
+        }
+        return dp[i][size] = Math.max(take,notTake);
     }
 }
