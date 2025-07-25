1143aa8017d25094993850d593b7b306b967afba
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-07-25 10:28:07 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (50.31%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSum(int[] nums) {
+        boolean[] map = new boolean[101];
+        int res = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i : nums){
+            max = Math.max(max,i);
+            if(i < 0){
+                continue;
+            }
+            if(!map[i]){
+                res += i;
+                map[i] = true;
+            }
+        }
+        if(max < 0){
+            return max;
+        }
+        return res;
+    }
+}
