973fc19132135960ac4cb2fe69e4fe88f1a6e039
venkatkalyan18 <107909052+venkatkalyan18@users.noreply.github.com>
2025-08-22 10:17:26 +0530

    Time: 6 ms (24.39%) | Memory: 155.5 MB (96.75%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,8 +1,8 @@
 class Solution {
     public int minimumArea(int[][] grid) {
         int[] hor = new int[2];
-        hor[0] = -1;
-        hor[1] = -1;
+        hor[0] = Integer.MAX_VALUE;
+        hor[1] = Integer.MIN_VALUE;
         int[] ver = new int[2];
         ver[0] = Integer.MAX_VALUE;
         ver[1] = Integer.MIN_VALUE;
@@ -10,34 +10,15 @@ class Solution {
 
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 1){
-                    hor[0] = i;
-                    break;
-                }
-            }
-            if(hor[0] != -1){
-                break;
-            }
-        }
 
-        for(int i=grid.length-1;i>=0;i--){
-            for(int j=grid[0].length-1;j>=0;j--){
-                if(grid[i][j] == 1){
-                    hor[1] = i;
-                    break;
-                }
-            }
-            if(hor[1] != -1){
-                break;
-            }
-        }
+                if(grid[i][j] == 0) continue;
 
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == 1){
-                    ver[0] = Math.min(ver[0],j);
-                    ver[1] = Math.max(ver[1],j);
-                } 
+                hor[0] = Math.min(hor[0],i);
+                hor[1] = Math.max(hor[1],i);
+
+                ver[0] = Math.min(ver[0],j);
+                ver[1] = Math.max(ver[1],j);
+
             }
         }
 
